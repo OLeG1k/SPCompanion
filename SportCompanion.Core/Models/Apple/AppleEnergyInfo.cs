b0f61ed133e147c5d41253f15fdd1c04f8e20ed7
Sergey Zhimerin <zhimerin98007@gmail.com>
2022-05-10 16:37:44 +0300

    Убрал лишнее

--- SportCompanion.Core/Models/Apple/AppleEnergyInfo.cs
+++ SportCompanion.Core/Models/Apple/AppleEnergyInfo.cs
@@ -3,14 +3,5 @@
     public class AppleEnergyInfo : AppleRecordInfo
     {
         public double Value { get; set; }
-
-        public static AppleEnergyInfo ConvertFrom(AppleRecordInfo appleRecordInfo)
-        {
-            return new AppleEnergyInfo
-            {
-                EndDate = appleRecordInfo.EndDate,
-                StartDate = appleRecordInfo.StartDate
-            };
-        }
     }
 }
--- SportCompanion.Core/Models/Apple/AppleStepInfo.cs
+++ SportCompanion.Core/Models/Apple/AppleStepInfo.cs
@@ -3,13 +3,5 @@
     public class AppleStepInfo : AppleRecordInfo
     {
         public int Value { get; set; }
-        public static AppleStepInfo ConvertFrom(AppleRecordInfo appleRecordInfo)
-        {
-            return new AppleStepInfo
-            {
-                EndDate = appleRecordInfo.EndDate,
-                StartDate = appleRecordInfo.StartDate
-            };
-        }
     }
 }
